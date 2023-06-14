7db544206e1c8a0cd45e11d351d80b593a6d5149
Marina <134020308+mzluka@users.noreply.github.com>
2023-06-14 23:28:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def set_png_as_page_bg(png_file):
 
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
-set_png_as_page_bg('8.JPG')
+set_png_as_page_bg('8.jpg')
 
 
 classifier_name=['XGBoost', 'Гадание на кофейной гуще']
