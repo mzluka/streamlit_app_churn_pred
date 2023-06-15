eac487d4373f6847a85e1fca2b14662f5de0510c
Marina <134020308+mzluka@users.noreply.github.com>
2023-06-15 04:56:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, Ha
 def main():
     st.title("Прогноз оттока клиентов из банка")
     html_temp = """
-    <div style="background-color:white ;padding:20px">
+    <div style="background-color:white ;padding:10px">
     <h2 style="color:red;text-align:center;">Введите данные по клиенту:</h2>
     </div>
     """
@@ -83,7 +83,7 @@ def main():
     unsafe_allow_html=True,
 )
 
-    CreditScore = st.slider('Скоринговый балл', 0, 400)
+    CreditScore = st.slider('Скоринговый балл', 300, 900)
 
     Geography = st.selectbox('Регион', ['France', 'Germany', 'Spain'])
     Geo = int(le1_pik.transform([Geography]))
@@ -95,7 +95,7 @@ def main():
 
     Tenure = st.selectbox("Продолжительность обслуживания в банке, лет", ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','10','11','12','13','14','15'])
 
-    Balance = st.slider("Баланс счёта", 0.00, 25000.00)
+    Balance = st.slider("Баланс счёта", 0.00, 250000.00)
 
     NumOfProducts = st.selectbox('Количество банковских продуктов', ['1', '2', '3', '4'])
 
@@ -103,7 +103,7 @@ def main():
 
     IsActiveMember = st.selectbox("Активность", ['0', '1'])
 
-    EstimatedSalary = st.slider("Заработная плата", 0.00, 25000.00)
+    EstimatedSalary = st.slider("Заработная плата", 0.00, 250000.00)
 
                 
     churn_html = """  
