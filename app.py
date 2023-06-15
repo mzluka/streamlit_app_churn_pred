0fd38841122bbb6102c63f73d1be1f9323cd48df
Marina <134020308+mzluka@users.noreply.github.com>
2023-06-15 03:05:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def main():
 
     Tenure = st.selectbox("Продолжительность обслуживания в банке, лет", ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','10'])
 
-    Balance = st.slider("Баланс счёта", 0.00, 250000.00)
+    Balance = st.slider("Баланс счёта", 0.00, 5000.00)
 
     NumOfProducts = st.selectbox('Количество банковских продуктов', ['1', '2', '3', '4'])
 
@@ -104,7 +104,7 @@ def main():
 
     IsActiveMember = st.selectbox("Активность клиента", ['0', '1'])
 
-    EstimatedSalary = st.slider("Заработная плата", 0.00, 200000.00)
+    EstimatedSalary = st.slider("Заработная плата", 0.00, 5000.00)
 
                 
     if int(Age)- int(Tenure)< 17:
