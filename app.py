8230085834e071b28b28acb498a9cc3b2cae4181
Marina <134020308+mzluka@users.noreply.github.com>
2023-06-17 21:11:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def main():
     unsafe_allow_html=True,
 )
 
-    CreditScore = st.number_input('Скоринговый балл', 0, 400)
+    CreditScore = st.number_input('Скоринговый балл')
 
     Geography = st.selectbox('Регион', ['France', 'Germany', 'Spain'])
     Geo = int(le1_pik.transform([Geography]))
