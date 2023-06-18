4dc1d801da97bd869e00aeb6d1d661274d04da38
Marina <134020308+mzluka@users.noreply.github.com>
2023-06-18 13:15:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -87,14 +87,8 @@ def main():
     Geography = st.selectbox('Регион', ['France', 'Germany', 'Spain'])
     Geo = int(le1_pik.transform([Geography]))
 
-    Gen = st.selectbox('Пол',  ['Женский', 'Мужской'])
-       
-    if Gen == 'Женский':
-        Gender = 0
-    else:
-        Gender = 1
-
-    Gen = int(le_pik.transform([Gender]))
+    Gender = st.selectbox('Организационно-правовая форма', ['Male', 'Female'])
+    Gen = int(le_pik.transform([Gender]))))
         
     Age = st.number_input('Возраст', min_value=18, max_value=92, format="%d")
 
