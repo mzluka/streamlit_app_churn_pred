09c47e521f54f4452de43204cd5a8f812da78b26
Marina <134020308+mzluka@users.noreply.github.com>
2023-06-18 13:45:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -84,9 +84,8 @@ def main():
   
     CreditScore = st.number_input('Скоринговый балл', min_value=1, max_value=400, format="%d")
 
-    Geography = st.selectbox('Регион', ['France', 'Germany', 'Spain'])
-    Geo = int(le1_pik.transform([Geography]))
-
+    Geo = st.selectbox('Регион', ['Гродно', 'Ошмяны', 'Лида'])
+    
     Gen = st.selectbox('Пол',  ['Женский', 'Мужской'])
     if Gen == 'Женский':
         Gender = 0
