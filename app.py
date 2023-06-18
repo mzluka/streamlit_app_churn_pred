26c4e562f315ac189800feec21fd67116e44c9d1
Marina <134020308+mzluka@users.noreply.github.com>
2023-06-18 13:21:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -38,14 +38,14 @@ st.subheader(option)
 
 
 #Importing model and label encoders
-model=pickle.load(open("final_xg_model.pkl","rb"))
-#model_1 = pickle.load(open("final_rf_model.pkl","rb"))
+model=pickle.load(open("final_LGBM_model.pkl","rb"))
+#model_1 = pickle.load(open("final_RF_model.pkl","rb"))
 le_pik=pickle.load(open("label_encoding_for_gender.pkl","rb"))
 le1_pik=pickle.load(open("label_encoding_for_geo.pkl","rb"))
 
 def predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary):
     input = np.array([[CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary]]).astype(np.float64)
-    if option == 'RandomForest':
+    if option == 'LightGMBClassifier':
         prediction = model.predict_proba(input)
         pred = '{0:.{1}f}'.format(prediction[0][0], 2)
 
