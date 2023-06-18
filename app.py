ea3b555aef19c29e4cc18f51e4dc3c81dbf4c959
Marina <134020308+mzluka@users.noreply.github.com>
2023-06-18 13:08:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def main():
   
     CreditScore = st.number_input('Скоринговый балл', min_value=1, max_value=400, format="%d")
 
-    Geography = st.selectbox('География/регион', ['Минск', 'Брест', 'Могилев'])
+    Geography = st.selectbox('Регион', ['Гродно', 'Ошмяны', 'Лида'])
 
     Gen = st.selectbox('Пол',  ['Женский', 'Мужской'])
     
