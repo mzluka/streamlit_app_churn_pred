a455e8227c4440744088be8c03f820c04d54605e
Marina <134020308+mzluka@users.noreply.github.com>
2023-06-18 13:36:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ st.subheader(option)
 
 
 #Importing model and label encoders
-model=pickle.load(open("final_LGBM_model.pkl","rb"))
-#model_1 = pickle.load(open("final_RF_model.pkl","rb"))
+model = pickle.load(open("final_LGBM_model.pkl","rb"))
+model_1 = pickle.load(open("final_RF_model.pkl","rb"))
 le_pik=pickle.load(open("label_encoding_for_gender.pkl","rb"))
 le1_pik=pickle.load(open("label_encoding_for_geo.pkl","rb"))
 
