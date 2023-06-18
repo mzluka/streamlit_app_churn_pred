2950247278f5859c52ca7f0f21477dee9e0f986b
Marina <134020308+mzluka@users.noreply.github.com>
2023-06-18 12:43:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import sklearn
 
-#from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from lightgbm import LGBMClassifier
 
 import streamlit as st
 import pickle
