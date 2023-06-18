866a076ddbd3055d72fd8452068867360ff0610b
Marina <134020308+mzluka@users.noreply.github.com>
2023-06-18 16:10:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, Ha
         prediction = model_2.predict_proba(input)
         pred = '{0:.{1}f}'.format(prediction[0][0], 2)
 
-    else:
+    №else:
         pred=0.30
         #st.markdown('Клиент остаётся в банке')
 
