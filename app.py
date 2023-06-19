03d3472a5ad12bef8a73c6540354f630c539dd24
Marina <134020308+mzluka@users.noreply.github.com>
2023-06-19 03:38:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import sklearn
 
 #from sklearn.linear_model import LogisticRegression
 #from sklearn.ensemble import RandomForestClassifier
-from lightgbm import LGBMClassifier
+#from lightgbm import LGBMClassifier
+import xgboost as xgb
 
 import streamlit as st
 import pickle
@@ -31,35 +32,30 @@ def set_png_as_page_bg(png_file):
 set_png_as_page_bg('5.jpg')
 
 
-classifier_name=['RandomForestClassifier', 'LightGMBClassifier']
+classifier_name=['LogisticRegression', 'RandomForestClassifier', 'LightGMBClassifier']
 option = st.sidebar.selectbox('Выберите алгоритм для прогнозирования', classifier_name)
 st.subheader(option)
 
 
 
 #Importing model and label encoders
-model = pickle.load(open("final_LGBM_model.pkl","rb"))
-#model_1 = pickle.load(open("final_RF_model.pkl","rb"))
-#model_2 = pickle.load(open("final_LR_model.pkl","rb"))
-model_3 = pickle.load(open("final_xg_model.pkl","rb"))
+model = pickle.load(open("final_LR_model.pkl","rb"))
+model_1 = pickle.load(open("final_RF_model.pkl","rb"))
+model_2 = pickle.load(open("final_LGBM_model.pkl","rb"))
 le_pik=pickle.load(open("label_encoding_for_gender.pkl","rb"))
 le1_pik=pickle.load(open("label_encoding_for_geo.pkl","rb"))
 
 def predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary):
     input = np.array([[CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary]]).astype(np.float64)
-    if option == 'LightGMBClassifier':
+    if option == 'LogisticRegression':
         prediction = model.predict_proba(input)
         pred = '{0:.{1}f}'.format(prediction[0][0], 2)
 
-    #if option == 'RandomForestClassifier':
-        #prediction = model_1.predict_proba(input)
-        #pred = '{0:.{1}f}'.format(prediction[0][0], 2)
-        
-    #if option == 'LogisticRegression':
-        #prediction = model_2.predict_proba(input)
-        #pred = '{0:.{1}f}'.format(prediction[0][0], 2)
-
     if option == 'RandomForestClassifier':
+        prediction = model_1.predict_proba(input)
+        pred = '{0:.{1}f}'.format(prediction[0][0], 2)
+        
+    if option == 'LightGMBClassifier':
         prediction = model_3.predict_proba(input)
         pred = '{0:.{1}f}'.format(prediction[0][0], 2)
 
