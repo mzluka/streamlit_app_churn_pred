57ba7ab87add723598553a24228b20cf879de756
Marina <134020308+mzluka@users.noreply.github.com>
2023-06-19 06:07:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,8 @@ def set_png_as_page_bg(png_file):
 set_png_as_page_bg('5.jpg')
 
 
-classifier_name=['LightGMBClassifier']
+classifier_name=['Цель проекта: построение модели оттока клиентов банка. Реализованы следующие задачи: препроцессинг, исследовательский анализ данных (EDA), визуализация, построение моделей для прогнозирования оттока на основе Logistic Regression, Random Forest, LightGBM, сравнение качества полученных моделей, 
+                  развёртывание модели с наибольшей предиктивной мощностью в StreamLit Cloud.']
 option = st.sidebar.selectbox('Алгоритм для прогнозирования оттока клиентов', classifier_name)
 st.subheader(option)
 
