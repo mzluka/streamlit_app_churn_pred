64e1f603aac0efef891402de259cad21155a00a1
Marina <134020308+mzluka@users.noreply.github.com>
2023-06-19 04:27:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def main():
     st.title("Прогноз оттока клиентов из банка")
     html_temp = """
     <div style="background-color:#f3f6f4 ;padding:10px">
-    <h2 style="color:#f6f3f4;text-align:center;">Введите данные по клиенту:</h2>
+    <h2 style="color:red;text-align:center;">Введите данные по клиенту:</h2>
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
